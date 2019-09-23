9032a3b8e758ea0f424b7715a9ccfb98e7ae0820
Jozsef Varga <jozsef.varga.123@gmail.com>
2019-09-23 11:47:49 +0200

    - Add new task - task done

--- TODO-APP/Program.cs
+++ TODO-APP/Program.cs
@@ -11,16 +11,22 @@ namespace TODO_APP
         static void Main(string[] args)
         {
             var print = new Print();
+            var taskHandler = new TaskHandler();
 
             if (args.Length == 0)
             {
-                
+
                 print.WithoutArgument();
             }
 
-            if (args.Contains("-l"))
+            else if (args[0] == "-l")
             {
                 print.ListTasks();
+            }
+
+            else if (args[0] == "-a")
+            {
+                taskHandler.AddTask(args[1]);
             }
         }
     }
---
+++ TODO-APP/TaskHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TODO_APP
+{
+    class TaskHandler
+    {
+        protected string listPath = @"D:\C#\Joco456-TO-DO-APP\TODO-APP\toDoList.txt";
+
+        public void AddTask(string task)
+        {
+            StreamWriter writer = new StreamWriter(listPath, true);
+
+            writer.WriteLine(task);
+            writer.Close();
+        }
+    }
+}
