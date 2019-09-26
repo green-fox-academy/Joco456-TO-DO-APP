9c4c638fa9a2652dd3fe1618307167858c028948
Jozsef Varga <jozsef.varga.123@gmail.com>
2019-09-26 11:41:17 +0200

    Program.cs refactored into small methods

--- TODO-APP/Program.cs
+++ TODO-APP/Program.cs
@@ -13,72 +13,109 @@ namespace TODO_APP
             var print = new Print();
             var taskHandler = new TaskHandler();
 
+            ArgumentHandler(args, print, taskHandler);            
+        }
+
+        public static void ArgumentHandler(string[] args, Print printObj, TaskHandler taskHandlerObj)
+        {
             if (args.Length == 0)
             {
-                print.WithoutArgument();
+                NoArgument(printObj);
             }
 
             else if (args[0] == "-l")
             {
-                print.ListTasks();
+                TaskLister(printObj);
             }
 
             else if (args[0] == "-a")
             {
-                try
-                {
-                    taskHandler.AddTask(args[1]);
-                }
-                catch (Exception)
-                {
-                    Console.WriteLine("Unable to add: no task provided");
-                }
+                Adder(args, taskHandlerObj);
             }
 
             else if (args[0] == "-c")
             {
-                if (args.Length == 1)
-                {
-                    Console.WriteLine("Unable to check: no index provided");
-                }
-                else
-                    try
-                    {
-                        taskHandler.CheckTask(Convert.ToInt32(args[1]));
-                    }
-                    catch (IndexOutOfRangeException)
-                    {
-                        Console.WriteLine("Unable to check: index is out of bound");
-                    }
-                    catch (FormatException)
-                    {
-                        Console.WriteLine("Unable to check: index is not a number");
-                    }
+                Checker(args, taskHandlerObj);
             }
 
             else if (args[0] == "-r")
             {
-                try
-                {
-                    taskHandler.RemoveTask(Convert.ToInt32(args[1]));
-                }
-                catch (IndexOutOfRangeException)
-                {
-                    Console.WriteLine("Unable to remove: no index provided");
-                }
-                catch (ArgumentOutOfRangeException)
-                {
-                    Console.WriteLine("Unable to remove: index is out of bound");
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("Unable to remove: index is not a number");
-                }
+                Remover(args, taskHandlerObj);
             }
 
             else if (args[0] != "-l" && args[0] != "-a" && args[0] != "-c" && args[0] != "-r")
             {
-                print.UnsupportedArgument();
+                BadArgument(printObj);
+            }
+        }
+
+        public static void NoArgument(Print printObj)
+        {
+            printObj.WithoutArgument();
+        }
+
+        public static void BadArgument(Print printObj)
+        {
+            printObj.UnsupportedArgument();
+        }
+
+        public static void TaskLister(Print printObj)
+        {
+            printObj.ListTasks();
+        }
+
+        public static void Adder(string[] args, TaskHandler taskHandlerObj)
+        {
+            try
+            {
+                taskHandlerObj.AddTask(args[1]);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to add: no task provided");
+            }
+        }
+
+        public static void Remover(string[] args, TaskHandler taskHandlerObj)
+        {
+            try
+            {
+                taskHandlerObj.RemoveTask(Convert.ToInt32(args[1]));
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Unable to remove: no index provided");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Unable to remove: index is out of bound");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Unable to remove: index is not a number");
+            }
+        }
+
+        public static void Checker(string[] args, TaskHandler taskHandlerObj)
+        {
+            if (args.Length == 1)
+            {
+                Console.WriteLine("Unable to check: no index provided");
+            }
+            else
+            {
+                try
+                {
+                    taskHandlerObj.CheckTask(Convert.ToInt32(args[1]));
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("Unable to check: index is out of bound");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Unable to check: index is not a number");
+                }
             }
         }
     }
